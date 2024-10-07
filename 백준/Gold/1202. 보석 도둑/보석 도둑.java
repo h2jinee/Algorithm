47bfdc0307ee0fe26d03fbe9de2d00c626e5a5fd
HEEJIN JEON <wjsgmlwls97@gmail.com>
2024-10-07 17:57:46 +0900

    [Gold II] Title: 보석 도둑, Time: 1968 ms, Memory: 134812 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -21,55 +21,49 @@ class Jewel implements Comparable<Jewel> {
     }
 }
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
-        // 0. 입력 및 초기화
+        // 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long sum = 0;
-        int idx = 0;
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         // 보석 수
         int N = Integer.parseInt(st.nextToken());
         // 가방 수
         int K = Integer.parseInt(st.nextToken());
 
-        Integer[] C = new Integer[K];
         List<Jewel> jewels = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
-            StringTokenizer jewel = new StringTokenizer(br.readLine());
-            // 보석 무게
-            int M = Integer.parseInt(jewel.nextToken());
-            // 보석 크기
-            int V = Integer.parseInt(jewel.nextToken());
-
-            jewels.add(new Jewel(M, V));
+            st = new StringTokenizer(br.readLine());
+            // 보석 무게, 크기를 pq에 저장
+            jewels.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
+        Integer[] C = new Integer[K];
         for (int i = 0; i < K; i++) {
-            StringTokenizer bag = new StringTokenizer(br.readLine());
             // 가방에 담을 수 있는 최대 무게
-            C[i] = Integer.parseInt(bag.nextToken());
+            C[i] = Integer.parseInt(br.readLine());
         }
 
         Collections.sort(jewels);
         Arrays.sort(C);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0; i < C.length; i++) {
-            while (idx < N && jewels.get(idx).M <= C[i]) {
+        long sum = 0;
+        int idx = 0;
+
+        for (int capacity : C) {
+            while (idx < N && jewels.get(idx).M <= capacity) {
                 pq.add(jewels.get(idx).V);
                 idx++;
             }
-
             if (!pq.isEmpty()) {
                 sum += pq.poll();
             }
         }
 
-        bw.write(sum + "");
+        bw.write(String.valueOf(sum));
         bw.close();
         br.close();
     }
