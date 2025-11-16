e816017c5ba774f6bc4fa395ec82393b30add614
HEEJIN JEON <wjsgmlwls97@gmail.com>
2025-11-16 18:57:29 +0900

    Time: 1 ms (84.51%), Space: 44 MB (7.56%) - LeetHub

--- LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ LeetCode/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,6 +1,10 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int max = Arrays.stream(candies).max().getAsInt();
+        int max = 1;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
         List<Boolean> answer = new ArrayList<>();
         for (int candy : candies) {
             if ((candy + extraCandies) >= max) {
