09908e1226186223604dd262571eab5c1afc227d
HEEJIN JEON <wjsgmlwls97@gmail.com>
2025-11-17 23:11:10 +0900

    Time: 1 ms (97.75%), Space: 47.7 MB (15.69%) - LeetHub

--- LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers.java
+++ LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,32 +1,14 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        boolean answer = false;
-
-        for (int i = 0; i < flowerbed.length - 1; i++) {
-            if (i == flowerbed.length - 2) {
-                if (flowerbed[i] == 0 && flowerbed[i+1] == 0) {
-                    flowerbed[i+1] = 1;
-                    n -= 1;
-                }
-                break;
-            }
-
-            if (i == 0) {
-                if (flowerbed[i] == 0 && flowerbed[i+1] == 0) {
-                    flowerbed[i] = 1;
-                    n -= 1;
-                }
-            } else {
-                if (flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) {
-                    flowerbed[i] = 1;
-                    n -= 1;
-                }
+        for(int i = 0; i < flowerbed.length; i++) {
+            if(
+                flowerbed[i] == 0 
+                && (i == 0 || flowerbed[i-1] == 0) 
+                && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                n--;
+                flowerbed[i] = 1;
             }
         }
-
-        if (flowerbed.length == 1 && flowerbed[0] == 0) answer = true;
-        if (n <= 0) answer = true;
-
-        return answer;
+        return n <= 0;
     }
 }
