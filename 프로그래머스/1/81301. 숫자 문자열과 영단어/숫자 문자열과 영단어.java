38156cb28099e0031dac7a0097d4b9ad4acbd490
HEEJIN JEON <wjsgmlwls97@gmail.com>
2025-11-18 21:08:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.57 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        String[] alphabet = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for (int i = 0; i < alphabet.length; i++) {
+            s = s.replaceAll(alphabet[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
