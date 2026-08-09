d36436a03f5dd0a03b4866a9f1354d1b2ec304aa
HEEJIN JEON <wjsgmlwls97@gmail.com>
2026-08-09 23:23:10 +0900

    [level 2] Title: H-Index, Time: 3.10 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,19 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        ArrayList<Integer> citationList = new ArrayList<>();
-
+        
+        Arrays.sort(citations);
+        
         for (int i = 0; i < citations.length; i++) {
-            citationList.add(citations[i]);
-        }
+            int count = citations.length - i;
 
-        citationList.sort(Collections.reverseOrder());
-
-        for (int i = 0; i < citationList.size(); i++) {
-            if (citationList.get(i) >= i + 1) {
-                answer++;
+            if (citations[i] >= count) {
+                return count;
             }
         }
+        
         return answer;
     }
 }
